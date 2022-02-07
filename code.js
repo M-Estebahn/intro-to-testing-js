f5c04d647555ca5fc8bf792ce4659f9b37fad969
Estebahn Molina <molina.estebahn@gmail.com>
2022-02-07 10:24:38 -0600

    refactor code for isVowel

--- code.js
+++ code.js
@@ -19,26 +19,14 @@ function isEven(input) {
     return (typeof input != 'boolean'&& input % 2 ===0);
 }
 
+// function isVowel(input){
+//     if (input ==='a'||'e'||'i'||'o'||'u'||'A'||'E'||'I'||'O'||'U')
+//         return true
+// }
 function isVowel(input){
-      switch (input){
-          case ('a'):
-              return true;
-          case ('A'):
-              return true;
-          case ('y'):
-              return false;
-          case (4):
-              return false;
-          case (true):
-              return false;
-          case (false):
-              return false;
-          case ('banana'):
-              return false;
-          case(undefined):
-              return false
-
-      }
-
+    if (input ==='a'||input ==='e'||input ==='i'||input ==='o'||input ==='u'||input ==='A'||input ==='E'||input ==='I'||input ==='O'||input ==='U')
+        return true
+    else
+        return false
 
 }
