86265e81c3528cc82c9d8b1842c6fb7b8ce88d82
Estebahn Molina <molina.estebahn@gmail.com>
2022-02-07 11:32:55 -0600

    refactor code for add function

--- code.js
+++ code.js
@@ -31,14 +31,8 @@ function isVowel(input){
 
 }
 function add(num1,num2){
-if(num1===2 && num2===3)
-    return 5
-else if (num1===(-3)&& num2===(-9))
-    return -12
-else if (num1==5 && num2==6)
-    return 11
-else if (num1==-4&&num2==10)
-    return 6
-else if (isNaN(num1)||isNaN(num2))
+ if (isNaN(num1)||isNaN(num2))
     return NaN
+  else
+      return Number(num1)+Number(num2)
 }
